a755bd46198b3a420486283a9c1147e4f7dc2a26
reign0f <johnfgraf@protonmail.ch>
2023-02-28 18:56:21 -0800

    Create WIn64_MD5hasher.py

---
+++ WIn64_MD5hasher.py
@@ -0,0 +1,15 @@
+import hashlib
+
+# Open the input and output files
+with open("output.txt", "r", encoding="utf-16") as infile, open("crackme.txt", "w") as outfile:
+    # Read each line from the input file
+    for line in infile:
+        # Remove any trailing whitespace
+        line = line.strip()
+        
+        # Calculate the MD5 hash of the line
+        hash_object = hashlib.md5(line.encode())
+        hash_hex = hash_object.hexdigest()
+        
+        # Write the hash to the output file
+        outfile.write(hash_hex + "\n")
